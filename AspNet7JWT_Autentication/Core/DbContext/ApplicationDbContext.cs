6d9b8f504f0f9c7a804e59219b3916e81915f01f
Maxruizdz <maxruizdz@hotmail.com>
2024-02-27 19:06:01 -0300

    implementando Base de Datos y Identity

---
+++ AspNet7JWT_Autentication/Core/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNet7JWT_Autentication.Core.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
